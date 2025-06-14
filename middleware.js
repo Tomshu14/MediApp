4f96930b1c8dba289f4e67691c2d29fee44f8ed5
Tomshu14 <tomishu14@gmail.com>
2025-06-14 10:32:25 +0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
